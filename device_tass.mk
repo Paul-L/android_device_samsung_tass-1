d34c6b7e3c6abbf138ab8e226f0bc017ce5e9e99
Erik Castricum <erikcas1972@gmail.com>
2013-01-15 00:30:21 +0100

    Update device_tass.mk

--- device_tass.mk
+++ device_tass.mk
@@ -35,9 +35,9 @@ PRODUCT_PACKAGES += \
 
 ## Ramdisk
 PRODUCT_COPY_FILES += \
-    device/samsung/tass/ramdisk/init.gt-s5660board.rc:root/init.gt-s5660board.rc \
-    device/samsung/tass/ramdisk/init.gt-s5660board.usb.rc:root/init.gt-s5660board.usb.rc \
-    device/samsung/tass/ramdisk/ueventd.gt-s5660board.rc:root/ueventd.gt-s5660board.rc \
+    device/samsung/msm7x27-common/ramdisk/init.device.rc:root/init.gt-s5660board.rc \
+    device/samsung/msm7x27-common/ramdisk/init.device.usb.rc:root/init.gt-s5660board.usb.rc \
+    device/samsung/msm7x27-common/ramdisk/ueventd.device.rc:root/ueventd.gt-s5660board.rc \
     device/samsung/tass/ramdisk/TASS.rle:root/TASS.rle
 
 ## LDPI assets
